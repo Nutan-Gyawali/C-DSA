478d5f88b8498168b4e9a7a6e5e92c9c7a2e4438
Nutan-Gyawali <nutangyawali01@gmail.com>
2025-12-01 20:55:08 +0545

    reverse array

---
+++ ReverseArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_in_C_
+{
+    internal class ReverseArray
+    {
+        public ReverseArray(int[] arr)
+        {
+            int len = arr.Length;
+            int[] newArr = new int[len];
+
+            //for (int i = len-2; i >=0; i--)
+            //{
+            //    newArr[i] = arr[i];
+            //}
+
+            int left = 0;
+            int right = len-1;
+
+            while (left < right)
+            {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+
+            }
+
+            for (int i = 0; i <len; i++)
+            {
+                Console.Write(arr[i]);
+            }
+        }
+    }
+}
