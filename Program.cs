b3ac066fc98115f0e07e7303e164b1ae59ad833e
Nutan-Gyawali <nutangyawali01@gmail.com>
2025-12-01 20:30:49 +0545

    first

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+// string revrse
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+
+        Console.WriteLine("enter a string");
+        string inout = Console.ReadLine();
+
+        string rev = "";
+        for (int i = inout.Length-1; i >= 0; i--) { 
+            rev += inout[i];
+        }
+
+        Console.WriteLine(rev);
+    }
+}
