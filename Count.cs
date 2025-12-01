2ba7a6c4a75b3ecceb0d5a75eefb102a2d59d675
Nutan-Gyawali <nutangyawali01@gmail.com>
2025-12-01 20:43:52 +0545

    count frequency

---
+++ Count.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_in_C_
+{
+    public class Count
+    {
+        public Count (string message)
+        {
+            Dictionary<char, int> dict = new Dictionary<char,int>();
+            foreach (char c in message)
+            {
+                if(dict.ContainsKey(c))
+                {
+                    dict[c]++;
+                }
+                else
+                {
+                    dict[c] = 1;
+                }
+            }
+             Console.WriteLine("\nCharacter Frequency:");
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+        }
+    }
+}
